45c4001b51532123764c2cce0ea5375c81577a50
Voodoo30fps <voodoo30fps@gmail.com>
2026-04-03 15:34:21 +0300

    added the first question

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,33 @@
 import { useState } from 'react';
 
+import QUESTIONS from '../questions.js';
+
 export default function Quiz() {
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
 
-  return <p>Currently active Question</p>;
+  const activeQuestionIndex = userAnswers.length;
+  // ^ storing it as sepparate state would be redundant, since it can be derived from the usersAnswers state
+
+  function handleSelectANswer(selectedAnswer) {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  }
+
+  return (
+    <div id='quiz'>
+      <div id='question'>
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id='answers'>
+          {QUESTIONS[activeQuestionIndex].answers.map((answer, index) => (
+            <li key={index} className='answer'>
+              <button onClick={() => handleSelectANswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
