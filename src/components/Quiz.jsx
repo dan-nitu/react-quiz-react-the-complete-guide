33e660980be56557ebf8e9ea14f295092e70234d
Voodoo30fps <voodoo30fps@gmail.com>
2026-04-03 15:50:23 +0300

    shuffled answers, added quiz completed screen

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
 import QUESTIONS from '../questions.js';
+import quizCompleteImg from '../assets/quiz-complete.png';
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
@@ -8,18 +9,33 @@ export default function Quiz() {
   const activeQuestionIndex = userAnswers.length;
   // ^ storing it as sepparate state would be redundant, since it can be derived from the usersAnswers state
 
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
   function handleSelectANswer(selectedAnswer) {
     setUserAnswers((prevUserAnswers) => {
       return [...prevUserAnswers, selectedAnswer];
     });
   }
 
+  if (quizIsComplete) {
+    return (
+      <div id='summary'>
+        <img src={quizCompleteImg} alt='Trophy icon' />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+  // ^ those 2 lines get executed only if the quiz is not complete.
+
   return (
     <div id='quiz'>
       <div id='question'>
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id='answers'>
-          {QUESTIONS[activeQuestionIndex].answers.map((answer, index) => (
+          {shuffledAnswers.map((answer, index) => (
             <li key={index} className='answer'>
               <button onClick={() => handleSelectANswer(answer)}>
                 {answer}
